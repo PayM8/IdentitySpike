c7e15ac520f3929bb250dd5441e8a27ab3dcd819
Mpho Majenge <mshauny@gmail.com>
2016-07-07 17:35:34 +0200

    added serviceAuthorization
    added serviceAuthorization

---
+++ Spike.Security/Authentication/BasicAuth/Hosting/CustomServiceAuthorizationManager.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Spike.Security.Authentication.BasicAuth.Hosting
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.ServiceModel;
+
+    class CustomServiceAuthorizationManager : ServiceAuthorizationManager
+    {
+        protected override bool CheckAccessCore(OperationContext operationContext)
+        {
+            return base.CheckAccessCore(operationContext);
+        }
+    }
+}
